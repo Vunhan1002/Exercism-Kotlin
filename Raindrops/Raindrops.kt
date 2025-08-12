e59d4e4fae814a9dfdaf9f53c5112ad3a3672308
Vunhan1002 <vunhan10022004@gmail.com>
2025-08-12 22:05:01 +0700

    Raindrops Exercise

---
+++ Raindrops/Raindrops.kt
@@ -0,0 +1,9 @@
+object Raindrops {
+    fun convert(n: Int): String {
+        val sb = StringBuilder()
+        if (n % 3 == 0) sb.append("Pling")
+        if (n % 5 == 0) sb.append("Plang")
+        if (n % 7 == 0) sb.append("Plong")
+        return if (sb.isEmpty()) n.toString() else sb.toString()
+    }
+}
